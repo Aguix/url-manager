5253d72ec489482bf9fca7a28abde4a40117a8f8
Aguix <aguix.contact+git@gmail.com>
2025-05-14 03:06:06 -0400

    CORE : config : add global CSS reset file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/fonts']
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/fonts'],
+  css: ['@/assets/css/reset.scss']
 })
