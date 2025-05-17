204fb15edb8bdd2a97f95989d24a7095ecd449d8
Aguix <aguix.contact+git@gmail.com>
2025-05-17 18:01:27 -0400

    FEAT : layout : add not connected header

---
+++ components/AppHeader.vue
@@ -0,0 +1,67 @@
+<template>
+    <header>
+        <NuxtLink to="/" class="link logo">
+            URL Manager
+        </NuxtLink>
+        <div class="buttons">
+            <NuxtLink to="/" class="link button">
+                Login
+            </NuxtLink>
+            <NuxtLink to="/" class="link button">
+                Register
+            </NuxtLink>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 3.5rem;
+    padding: 1rem;
+    background-color: $primary-color;
+    
+    font-family: 'Inter', sans-serif;
+
+    .link {
+        text-decoration: none;
+    }
+
+    .logo {
+        color: $secondary-color;
+        font-size: 1.7rem;
+    }
+
+    .buttons {
+        display: flex;
+        gap: 1rem;
+
+        .button {
+            padding: 0.4rem;
+            background-color: $secondary-color;
+            color: $primary-color;
+
+            border-radius: 0.6rem;
+            border-style: solid;
+            border-width: 0.2rem;
+            border-color: $primary-color;
+            
+            font-size: 1rem;
+            
+            cursor: pointer;
+
+            transition: all 1s ease; 
+
+            &:hover {
+                background-color: $primary-color;
+                color: $secondary-color;
+                border-color: $secondary-color;
+            }
+        }
+    }
+}
+</style>
