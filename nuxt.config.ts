6334c54c4e81dfb9e29eb3ef10cb61aae8bc57f0
Aguix <aguix.contact+git@gmail.com>
2025-05-19 19:38:02 -0400

    CORE : deps : add Prisma module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/fonts', '@prisma/nuxt'],
   css: ['@/assets/css/reset.scss'],
   vite: {
     css: {
