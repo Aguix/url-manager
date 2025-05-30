ed2f3b8e637a4db8faf52239d730059009e0a777
Aguix <aguix.contact+git@gmail.com>
2025-05-30 06:41:11 -0400

    FEAT : visit : save visit

--- server/api/redirection/[alias].get.ts
+++ server/api/redirection/[alias].get.ts
@@ -22,19 +22,36 @@ export default defineEventHandler(async (event) => {
         });
     }
 
+    if (!redirection.userId) return sendRedirect(event, redirection.url, 301);
 
+    const user = await prisma.user.findUnique({
+        where: { id: redirection.userId },
+        select: { id: true, username: true, email: true }
+    });
+    if (!user) return sendRedirect(event, redirection.url, 301);
+    
     const headers = getRequestHeaders(event);
     const parser = new UAParser.UAParser(headers['user-agent'] || '');
-    const ua = parser.getResult();
-    const browser = ua.browser.name || 'unknown';
-    const os = ua.os.name || 'unknown';
-    const platform = ua.device.type || 'desktop';
-    const ip = getRequestIP(event) || 'unknown';
-    const ip2 = event.context.clientAddress || 'unknown';
-    const ip3 = event.context.ip || 'unknown';
+    const userAgent = parser.getResult();
+    const browser = userAgent.browser.name || 'unknown';
+    const os = userAgent.os.name || 'unknown';
+    const platform = userAgent.device.type || 'desktop';
+    const ipAddress = getRequestIP(event) || 'unknown';
 
-    console.log(`Redirection accessed: ${redirection.alias} from IP: ${ip}, IP2: ${ip2}, IP3 : ${ip3}, Browser: ${browser}, OS: ${os}, Platform: ${platform}`);
+    console.log(`Redirection accessed: ${redirection.alias} from IP: ${ipAddress}, Browser: ${browser}, OS: ${os}, Platform: ${platform}`);
 
+
+    prisma.visit.create({
+        data: {
+            redirectionId: redirection.id,
+            ipAddress,
+            browser,
+            os,
+            platform
+        },
+    }).catch(err => {
+        console.error('Failed to log visitor:', err);
+    });
 
     return sendRedirect(event, redirection.url, 301);
 });
